7902d5e177a289987bf0e8cfd05b6bc5b3208124
aleksandrsVizins <zandergrunge@gmail.com>
2024-01-18 21:25:52 +0200

    basic select releases

--- project.py
+++ project.py
@@ -25,14 +25,61 @@ wait.until(EC.presence_of_element_located((By.CLASS_NAME, "page_charts_settings_
 # make it user-defined
 rating = "Popular"
 
-rating_div = driver.find_element(By.XPATH, "//div[@class = 'page_chart_query_item page_chart_query_item_type_selector']")
+rating_div = driver.find_element(By.XPATH, "//div[@onclick='RYMchart.openChartTypeSelect();']")
 rating_div.click()
-
-time.sleep(5)
 
 option = driver.find_element(By.XPATH, f"//div[@data-description='{rating}']")
 option.click()
 
+# time.sleep(5)
+
+
+# select the type
+
+types = []
+
+def select_release_type(release_type):
+    release_type_div = driver.find_element(By.XPATH, f"//div[@onclick=\"RYMchart.toggleReleaseType('{release_type}');\"]")
+    release_type_div.click()
+    types.append(release_type)
+
+release_type_div = driver.find_element(By.XPATH, "//div[@onclick='RYMchart.openObjectTypeSelect();']")
+release_type_div.click()
+
+# unselect beforehand
+elements = driver.find_elements(By.CLASS_NAME, "release_type_btn.selected")
+for element in elements:
+    element.click()
+
 time.sleep(5)
 
 
+# select releases
+
+select_release_type("album")
+select_release_type("ep")
+select_release_type("mixtape")
+
+# Click on the close button
+close_button = driver.find_element(By.XPATH, "//a[@onclick='RYMchart.closeObjectTypeSelect();']")
+close_button.click()
+
+# main_releases_div = driver.find_element(By.XPATH, "//a[@onclick='RYMchart.selectReleaseTypeMain();']")
+# main_releases_div.click()
+# types.append("Main")
+
+# select_release_type("album")
+# select_release_type("ep")
+# select_release_type("mixtape")
+# select_release_type("djmix")
+# select_release_type("single")
+# select_release_type("comp")
+
+# if "Main" in types:
+#     selected_types = "Main"
+# else:
+#     selected_types = ", ".join(types)
+
+
+
+time.sleep(5)
